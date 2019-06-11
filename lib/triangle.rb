f64505369f0fd2667e896e7959d920d1052ef96f
boruch d <dubywider@gmail.com>
2019-06-11 16:01:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
    case (@a <= 0 ||  @b <= 0 || @c <= 0)
    raise TriangleError
    when (@a+@b <= @c) || (@a+@c <= @b) || (@b+@c <= @a)
+     raise TriangleError
      
   class TriangleError < StandardError
     # triangle error code
