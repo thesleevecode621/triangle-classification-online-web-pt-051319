5b5382c5abcc6a9ee98d2a5fa6193a76743ae876
boruch d <dubywider@gmail.com>
2019-06-11 16:01:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,14 @@
 class Triangle
  attr_accessor 
  def initialize(a,b,c)
-   @A = a
+   @a = a
    @b = b
    @c = c
  end 
  def kind()
-   case (a <= 0 ||  a <= 0 || a <= 0)
+   case (@a <= 0 ||  @b <= 0 || @c <= 0)
    raise TriangleError
-   when (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+   when (@a+@b <= @c) || (@a+@c <= @b) || (@b+@c <= @a)
   class TriangleError < StandardError
     # triangle error code
   end
