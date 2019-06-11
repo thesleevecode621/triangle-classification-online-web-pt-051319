8fff789ec0604172a11aab0e2ae5f74cfe8e295d
boruch d <dubywider@gmail.com>
2019-06-11 16:10:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,23 +1,29 @@
 class Triangle
-  
- def initialize(a,b,c)
-   @a = a
-   @b = b
-   @c = c
- end 
- def kind()
-   case (@a <= 0 ||  @b <= 0 || @c <= 0)
-   raise TriangleError
-   when (@a+@b <= @c) || (@a+@c <= @b) || (@b+@c <= @a)
-     raise TriangleError
-   when  (@a == @b) && (@b == @c)
-        :equilateral
-  when (@a == @b) || (@b == @c) || (@a == @c)
-    :isosceles
-      when (@a != @b) && (@b != @c) && (@a != @c)
-        :scalene
-      end 
-    end 
-  class TriangleError < StandardError
-    # triangle error code
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+  # triangle error code
+end
