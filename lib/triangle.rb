5d4c8fbc3e87aa82817fc796a8de599c9a517909
boruch d <dubywider@gmail.com>
2019-06-11 15:55:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
  attr_accessor 
- def initialize
+ def initialize(a,b,c)
+   @A = a
+   @b = b
+   @c = c
+ end 
+ def kind 
   class TriangleError < StandardError
     # triangle error code
   end
